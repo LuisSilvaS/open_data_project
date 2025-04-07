8fb807251e88238f956dbf663e5bba2693752c05
lcsil <luis.silva.702@ufrn.edu.br>
2025-04-07 19:16:10 -0300

    corrigindo nrows

--- ibama/app.py
+++ ibama/app.py
@@ -1,20 +1,17 @@
-# streamlit_app.py
 import streamlit as st
 import pandas as pd
 
-# URL dos dados
 default_url = "https://lcsbkt.s3.us-east-2.amazonaws.com/bronze/vw_brasil_adm_auto_infracao_p.csv"
 
-# Título do app
-st.title("Visualizador de Dados - Autos de Infração")
+st.title("Visualizador de Autos de Infração - IBAMA")
 
-# Carregando os dados
 @st.cache_data
 def carregar_dados(url):
-    return pd.read_csv(url, sep=",", encoding="utf-8")
+    return pd.read_csv(url, sep=",", encoding="utf-8", nrows=500, low_memory=False)
 
-df = carregar_dados(default_url)
-
-# Mostrar as primeiras linhas
-st.subheader("Visualização dos primeiros registros")
-st.dataframe(df.head())
+try:
+    df = carregar_dados(default_url)
+    st.subheader("Visualização dos primeiros registros")
+    st.dataframe(df.head())
+except Exception as e:
+    st.error(f"Erro ao carregar os dados: {e}")
